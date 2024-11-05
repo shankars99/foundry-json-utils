c68135c98b16233ede2d1be0344867f35a1871f9
shankar <shankar@layerzerolabs.org>
2024-11-05 02:41:22 -0800

    rename: json/json -> json/utils
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- src/LZUtils.sol
+++ src/LZUtils.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
-import {JsonUtils} from "./json/json.sol";
+import {JsonUtils} from "./json/utils.sol";
 
 abstract contract LZUtils is JsonUtils {
     function IS_WORKING() public pure returns (bool) {
