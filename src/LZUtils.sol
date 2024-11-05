3ec2775f591113fffca0c5fc4dbd4f11dcd73bd9
shankar <shankar@layerzerolabs.org>
2024-11-05 02:44:11 -0800

    init: DVN and Executor utils
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- src/LZUtils.sol
+++ src/LZUtils.sol
@@ -2,8 +2,10 @@
 pragma solidity ^0.8.22;
 
 import {JsonUtils} from "./json/utils.sol";
+import {ExecutorUtils} from "./executor/utils.sol";
+import {DVNUtils} from "./dvn/utils.sol";
 
-abstract contract LZUtils is JsonUtils {
+abstract contract LZUtils is JsonUtils, ExecutorUtils, DVNUtils {
     function IS_WORKING() public pure returns (bool) {
         return true;
     }
